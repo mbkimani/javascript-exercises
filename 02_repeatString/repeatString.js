6a3dfb1e4a288fe209eaafb9440ea43541ccddc5
Brian Kimani <mbkimani@gmail.com>
2022-06-17 09:08:18 +0300

    given a string and n times to repeat it, this package repeats a string n times

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function (theString, repeats) {
+  let compoundString = "";
+  if (repeats < 0) {
+    compoundString = "ERROR";
+  }
 
+  for (let i = 0; i < repeats; i++) {
+    compoundString += theString;
+  }
+  return compoundString;
 };
 
 // Do not edit below this line
