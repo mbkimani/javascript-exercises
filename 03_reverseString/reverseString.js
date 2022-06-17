d06e547d433f4679190491f80c7bbea111000473
Brian Kimani <mbkimani@gmail.com>
2022-06-17 12:35:13 +0300

    reverse any string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function (aString) {
+  let splitString = aString.split("");
+  let joinString = [];
+  for (let i = splitString.length - 1; i >= 0; i--) {
+    joinString.push(splitString[i]);
+  }
 
+  return joinString.join("").toString();
 };
 
 // Do not edit below this line
