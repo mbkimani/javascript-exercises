bd100a3c3fa5c0ba323b147863ec5bf5f9271243
Brian Kimani <mbkimani@gmail.com>
2022-06-17 14:18:13 +0300

    remove an element(s) from an array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function (anArray, ...args) {
+  let elements = args;
+  let newArray = [];
+  outer: for (let i = 0; i < anArray.length; i++) {
+    for (let j = 0; j < elements.length; j++) {
+      if (anArray[i] === elements[j]) {
+        continue outer;
+      }
+    }
+    newArray.push(anArray[i]);
+  }
 
+  return newArray;
 };
 
 // Do not edit below this line
