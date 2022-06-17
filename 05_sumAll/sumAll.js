d484192b52471a991b2f0cdc4c7cd04ba1d2ab80
Brian Kimani <mbkimani@gmail.com>
2022-06-17 14:49:16 +0300

    add all the numbers between arguments

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function (a, b) {
+  let sum = 0;
+  if (a < 0 || b < 0 || typeof a !== "number" || typeof b !== "number") {
+    return "ERROR";
+  }
+  if (a > b) {
+    for (let i = b; i <= a; i++) {
+      sum += i;
+    }
+  } else {
+    for (let i = a; i <= b; i++) {
+      sum += i;
+    }
+  }
+  return sum;
 };
 
 // Do not edit below this line
