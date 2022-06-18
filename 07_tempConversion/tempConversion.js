360fc96b79e32c9598904ad48e822ab477a99748
Brian Kimani <mbkimani@gmail.com>
2022-06-18 06:54:11 +0300

    temp converter celsius and fahrenheit

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,24 @@
-const ftoc = function() {
+const ftoc = function (tempFahrenTemp) {
+  let celsiusTemp = (tempFahrenTemp - 32) * (5 / 9);
 
+  if (celsiusTemp == Math.round(celsiusTemp)) {
+    return celsiusTemp;
+  }
+
+  return Number(celsiusTemp.toFixed(1));
 };
 
-const ctof = function() {
+const ctof = function (tempCelsius) {
+  let fahrenTemp = tempCelsius * (9 / 5) + 32;
 
+  if (fahrenTemp == Math.round(fahrenTemp)) {
+    return fahrenTemp;
+  }
+
+  return Number(fahrenTemp.toFixed(1));
 };
-
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
